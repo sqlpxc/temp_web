c8cfd33bcf0671d02469220220c2e3ec0f381d15
sqlpxc <38583139+sqlpxc@users.noreply.github.com>
2019-12-30 01:20:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var connect = rere("connect");
 var serveStatic = require("serve-static");
 
 var app = connect();
-app.use(serveStatic("/"));
+app.use(serveStatic("."));
 
 app.listen(process.env.PORT || 8000);
  
