e9ef9e67f2d0d0347a683f226077f37bc5922438
sqlpxc <38583139+sqlpxc@users.noreply.github.com>
2019-12-30 12:02:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const http=require("http");
+const Path=require("path");
+const fs=require("fs");
 
 var server=http.createServer(function (req,res){
     fs.createReadStream(Path.resolve(__dirname,"."+req.url)).pipe(res);
