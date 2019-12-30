521b84d751a9ce7b3a6fd8625025d02c06f05f0a
sqlpxc <38583139+sqlpxc@users.noreply.github.com>
2019-12-30 12:08:23 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const http=require("http");
-const Path=require("path");
-const fs=require("fs");
-
-var server=http.createServer(function (req,res){
-    fs.createReadStream(Path.resolve(__dirname,"."+req.url)).pipe(res);
-})
